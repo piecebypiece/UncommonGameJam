21d272a72f0d442c29d1960c5bae08dc5533dfbf
김노을(Laptop) <commcencer@gmail.com>
2023-07-28 23:42:16 +0900

    Photon Player Testing

--- Assets/Script/Character/PlayerController.cs
+++ Assets/Script/Character/PlayerController.cs
@@ -1,14 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
 public class PlayerController : MoveCore
 {
     [SerializeField] protected Rigidbody rigid;
+    [SerializeField] PhotonView PV;
 
     protected override void Awake()
     {
         TryGetComponent(out rigid);
+    }
+
+    public void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+
     }
 
     protected override void FixedUpdate()
