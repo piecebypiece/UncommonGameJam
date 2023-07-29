58ef3ca9a0e904006de21c9eff190ef6166213cf
taeuk_choi <chlx357@gmail.com>
2023-07-29 11:57:10 +0900

    playerdatamanager

---
+++ Assets/Script/PlayerGameData.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+public class PlayerGameData
+{
+    public int wordAddIndex = 0;
+    public List<string> wordKeyList;
+    public List<string> localizeWordKeyList;
+
+    public void AddWordList(string key)
+    {
+        if(wordAddIndex == wordKeyList.Count)
+        {
+            wordAddIndex = 0;
+        }
+        wordKeyList[wordAddIndex] = key;
+        wordAddIndex++;
+    }
+
+
+    // 플레이어가 word의 키값을 번역할 수 있으면 번역되고 아니면 원문반환
+    public string LocalizeGameWordKey(string key)
+    {
+        if (localizeWordKeyList.Contains(key))
+            return GameLocalizeManager.Inst.Localize(key);
+        else
+            return GameLocalizeManager.Inst.Unlocalize(key);
+    }
+}
