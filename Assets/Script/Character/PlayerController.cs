8a78d136d977f7a4bef944cac7d22a5d9bf8ae56
김노을(Laptop) <commcencer@gmail.com>
2023-07-29 20:38:00 +0900

    중간저장

--- Assets/Script/Character/PlayerController.cs
+++ Assets/Script/Character/PlayerController.cs
@@ -8,6 +8,7 @@ public class PlayerController : MoveCore, IPunInstantiateMagicCallback
     [SerializeField] protected Rigidbody rigid;
     [SerializeField] public PhotonView PV;
     [SerializeField] GameObject inputManager;
+    private bool isChack = false;
 
     protected override void Awake()
     {
@@ -49,6 +50,31 @@ public class PlayerController : MoveCore, IPunInstantiateMagicCallback
             isTurn.Value = true;
             Turn();
         }
+
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            AllPlayerCollideChack();
+        }
+    }
+
+    private void AllPlayerCollideChack()
+    {
+        GameObject _playManager = GameObject.Find("PlayManager");
+        _playManager.TryGetComponent(out PlayManager playManager);
+
+        for (int i = 0; i < playManager.playerConList.Count - 1; i++)
+        {
+            float dist = Vector3.Distance(playManager.playerConList[i].transform.position, playManager.playerConList[i + 1].transform.position);
+            if (dist > playManager.GameEndDist)
+            {
+                isChack = true;
+            }
+        }
+    }
+
+    private void EndChack()
+    {
+        
     }
 
     public void OnPhotonInstantiate(PhotonMessageInfo info)
